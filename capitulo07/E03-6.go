404dc70c2291740cef988f13ef4f6a019056b502
Joel Amálio <joel.amalio@gmail.com>
2021-11-09 14:57:59 -0300

    Cap. 7 – Exercícios: Nível #3 – 6

---
+++ capitulo07/E03-6.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var nota float64 = 10
+
+	if nota == 10 {
+		fmt.Printf("Parabéns!!! Você é muito bom! Tirou a nota máxima e foi aprovado")
+	}
+}
