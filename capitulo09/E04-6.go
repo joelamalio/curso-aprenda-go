e460cfbbe083c90c6329c59ab07580875fdd9bb5
Joel Amálio <joel.amalio@gmail.com>
2021-11-10 16:03:00 -0300

    Cap. 9 – Exercícios: Nível #4 – 6

---
+++ capitulo09/E04-6.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	estados := make([]string, 27, 27)
+
+	estados = []string{
+		"Acre",
+		"Alagoas",
+		"Amapá",
+		"Amazonas",
+		"Bahia",
+		"Ceará",
+		"Distrito Federal",
+		"Espírito Santo",
+		"Goiás",
+		"Maranhão",
+		"Mato Grosso",
+		"Mato Grosso do Sul",
+		"Minas Gerais",
+		"Pará",
+		"Paraíba",
+		"Paraná",
+		"Pernambuco",
+		"Piauí",
+		"Rio de Janeiro",
+		"Rio Grande do Norte",
+		"Rio Grande do Sul",
+		"Rondônia",
+		"Roraima",
+		"Santa Catarina",
+		"São Paulo",
+		"Sergipe",
+		"Tocantins",
+	}
+	fmt.Println("len:", len(estados), "cap:", cap(estados))
+
+	for i := 0; i < len(estados); i++ {
+		fmt.Println(estados[i])
+	}
+
+}
