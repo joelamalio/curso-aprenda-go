16b4fd3d98c3201cdde1f0e3f5a1e62e72298e90
Joel Amálio <joel.amalio@gmail.com>
2021-11-25 19:55:36 -0300

    Cap. 15 – Exercícios: Nível #7 – 1

---
+++ capitulo15/E07-1.go
@@ -0,0 +1,17 @@
+/*
+- Crie um valor e atribua-o a uma variável.
+- Demonstre o endereço deste valor na memória.
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	x := 51
+
+	fmt.Println(&x)	
+}
+
